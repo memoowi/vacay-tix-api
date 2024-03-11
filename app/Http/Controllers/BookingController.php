4b8592258fe8393ec8257d27617c189d88c7aedf
Misbachul Munir Al Haddad Alwi <megatenlike@gmail.com>
2024-03-11 18:33:11 +0700

    feat:booking index

--- app/Http/Controllers/BookingController.php
+++ app/Http/Controllers/BookingController.php
@@ -2,9 +2,27 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Booking;
 use Illuminate\Http\Request;
 
 class BookingController extends Controller
 {
-    //
+    public function index(Request $request)
+    {
+        $bookings = $request->user()->bookings;
+        return response()->json([
+            'status' => 'success',
+            'data' => $bookings
+        ], 200);
+    }
+
+    public function indexAdminOnTour(Request $request)
+    {
+        $tour = $request->tour_id;
+        $bookings = Booking::where('tour_id', $tour)->get();
+        return response()->json([
+            'status' => 'success',
+            'data' => $bookings
+        ], 200);
+    }
 }
