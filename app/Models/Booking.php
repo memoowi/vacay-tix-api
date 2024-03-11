55131d2643eeaaefd421ac9ec11860c5c0d31b77
Misbachul Munir Al Haddad Alwi <megatenlike@gmail.com>
2024-03-11 09:30:21 +0700

    first commit

---
+++ app/Models/Booking.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasOne;
+
+class Booking extends Model
+{
+    use HasFactory;
+
+    protected $guarded = [];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+    public function tour(): BelongsTo
+    {
+        return $this->belongsTo(Tour::class);
+    }
+    public function payment(): HasOne
+    {
+        return $this->hasOne(Payment::class);
+    }
+}
